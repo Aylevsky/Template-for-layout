0991c0cbd951e715acbc09fe807ea18f4d5e05ae
aylevsky <aylevsky@gmail.com>
2025-06-13 11:04:16 +0300

    v_2.2.0 Перенесен стандартный набор стилей (миксины, переменные, функции). Исправлены ошибки для версии Dart SCSS 3.

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -9,6 +9,7 @@ import postcss from 'gulp-postcss';
 import postcssPresetEnv from 'postcss-preset-env';
 import postcssGroupMedia from 'postcss-sort-media-queries';
 import sourcemaps from 'gulp-sourcemaps';
+import bulkSass from 'gulp-sass-glob-use-forward';
 
 import { filePaths } from '../config/paths.js';
 import { plugins } from '../config/plugins.js';
@@ -17,31 +18,31 @@ import { logger } from '../config/logger.js';
 const sass = gulpSass(dartSass);
 
 export const scss = (isBuild, serverInstance) => {
-	const webpConfig = {
-		webpClass: '.webp',
-		noWebpClass: '.no-webp',
-	};
+  const webpConfig = {
+    webpClass: '.webp',
+    noWebpClass: '.no-webp',
+  };
 
-	return gulp.src(filePaths.src.scss)
-		.pipe(logger.handleError('SCSS'))
+  return gulp.src(filePaths.src.scss)
+    .pipe(logger.handleError('SCSS'))
+    .pipe(bulkSass())
+    .pipe(plugins.if(!isBuild, sourcemaps.init()))
+    .pipe(sass({ outputStyle: 'expanded' }, null))
+    .pipe(plugins.replace(/@img\//g, '../images/'))
 
-		.pipe(plugins.if(!isBuild, sourcemaps.init()))
-		.pipe(sass({ outputStyle: 'expanded' }, null))
-		.pipe(plugins.replace(/@img\//g, '../images/'))
+    .pipe(plugins.if(isBuild, webpCss(webpConfig)))
+    .pipe(plugins.if(isBuild, postcss([
+      autoprefixer(),
+      postcssPresetEnv(),
+      postcssGroupMedia({ sort: 'desktop-first' }),
+    ])))
 
-		.pipe(plugins.if(isBuild, webpCss(webpConfig)))
-		.pipe(plugins.if(isBuild, postcss([
-			autoprefixer(),
-			postcssPresetEnv(),
-			postcssGroupMedia({ sort: 'desktop-first' }),
-		])))
+    /** Раскомментировать если нужен не сжатый дубль файла стилей */
+    // .pipe(gulp.dest(filePaths.build.css))
 
-		/** Раскомментировать если нужен не сжатый дубль файла стилей */
-		// .pipe(gulp.dest(filePaths.build.css))
-
-		.pipe(plugins.if(isBuild, cleanCss()))
-		.pipe(rename({ extname: '.min.css' }))
-		.pipe(plugins.if(!isBuild, sourcemaps.write('.')))
-		.pipe(gulp.dest(filePaths.build.css))
-		.pipe(serverInstance.stream());
+    .pipe(plugins.if(isBuild, cleanCss()))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(plugins.if(!isBuild, sourcemaps.write('.')))
+    .pipe(gulp.dest(filePaths.build.css))
+    .pipe(serverInstance.stream());
 };
