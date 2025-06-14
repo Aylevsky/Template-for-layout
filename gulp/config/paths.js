cc467b89106e54c6969fc1297f4e6381de27fe6b
aylevsky <aylevsky@gmail.com>
2025-06-14 10:03:36 +0300

    v_2.3.4 Перенесены скрипты из старого шаблона.

--- gulp/config/paths.js
+++ gulp/config/paths.js
@@ -5,14 +5,14 @@ const srcFolder = './src';
 
 export const filePaths = {
   build: {
-    js: `${buildFolder}/js/`,
+    js: `${buildFolder}/scripts/`,
     css: `${buildFolder}/css/`,
     images: `${buildFolder}/images/`,
     fonts: `${buildFolder}/fonts/`,
     static: `${buildFolder}/static/`,
   },
   src: {
-    js: `${srcFolder}/js/*.js`,
+    js: `${srcFolder}/scripts/*.js`,
     images: `${srcFolder}/images/**/*.{jpg,jpeg,png,gif,webp}`,
     svg: `${srcFolder}/images/**/*.svg`,
     scss: [`${srcFolder}/styles/main.scss`, `${srcFolder}/pages/**/*.scss`],
@@ -24,7 +24,7 @@ export const filePaths = {
     fonts: `${srcFolder}/fonts/`,
   },
   watch: {
-    js: `${srcFolder}/js/**/*.js`,
+    js: `${srcFolder}/scripts/**/*.js`,
     scss: `${srcFolder}/**/*.scss`,
     html: `${srcFolder}/**/*.html`,
     pug: `${srcFolder}/**/*.pug`,
