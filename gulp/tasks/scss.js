a84b57cf5af42013d0c364ee77cf03e1c4b84874
aylevsky <aylevsky@gmail.com>
2025-06-14 09:56:25 +0300

    v_2.3.3 Исправены ошибки в работе SCSS.

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -13,7 +13,7 @@ import postcss from 'gulp-postcss';
 import postcssPresetEnv from 'postcss-preset-env';
 import postcssGroupMedia from 'postcss-sort-media-queries';
 import sourcemaps from 'gulp-sourcemaps';
-import bulkSass from 'gulp-sass-glob-use-forward';
+import sassGlob from 'gulp-sass-glob';
 
 import { filePaths } from '../config/paths.js';
 import { plugins } from '../config/plugins.js';
@@ -29,10 +29,22 @@ export const scss = (isBuild, serverInstance) => {
 
   return gulp.src(filePaths.src.scss)
     .pipe(logger.handleError('SCSS'))
-    .pipe(bulkSass())
+    .pipe(sassGlob())
     .pipe(plugins.if(!isBuild, sourcemaps.init()))
     .pipe(sass({ outputStyle: 'expanded' }, null))
-    .pipe(plugins.replace(/@img\//g, '../images/'))
+
+    /**
+     * Сокращения
+     */
+    // Путь к картинкам
+    .pipe(plugins.replace(/@img\//g, '../../../images/'))
+
+    // Сокращение миксина размера
+    .pipe(plugins.replace('@size', `@include size`))
+    .pipe(plugins.replace('@ad-size', `@include ad-size`))
+    .pipe(plugins.replace('@line-height', `@include line-height`))
+    .pipe(plugins.replace('@ad', `@include adaptiv-value`))
+    .pipe(plugins.replace('@med', `@include media`))
 
     .pipe(plugins.if(isBuild, webpCss(webpConfig)))
     .pipe(plugins.if(isBuild, postcss([
